83b9f59dace0afd19065cbc28b845ab3c8d6c390
Ivan <thefullarcticfox@users.noreply.github.com>
2021-03-25 15:29:36 +0300

    todo app

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,5 @@
 class TodosController < ApplicationController
+  skip_before_action :verify_authenticity_token
   before_action :set_todo, only: %i[ show edit update destroy ]
 
   # GET /todos or /todos.json
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,9 +3,22 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import Rails from "@rails/ujs";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
 
-Rails.start()
-ActiveStorage.start()
+let $ = require("jquery");
+let _ = require("underscore");
+let Backbone = require("backbone");
+
+console.log($.name)
+console.log("Underscore v" + _.VERSION);
+console.log("Backbone v" + Backbone.VERSION);
+
+Rails.start();
+ActiveStorage.start();
+
+// load on DOM ready
+$(function () {
+
+});
